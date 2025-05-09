98bc722633d646338810e24059cd2dbf4d95a368
DamClover <114771411+damclover@users.noreply.github.com>
2025-05-09 18:00:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,18 @@ DESTINATION="/usr/local/bin/metascan"
 
 # Install required tools
 echo "[INFO] Installing required tools..."
-sudo apt update && sudo apt install -y exiftool ffmpeg poppler-utils binwalk
+# Instalar pacotes Python
+pip install binwalk ffmpeg-python
+
+# Instalar ferramentas de linha de comando
+sudo apt update
+sudo apt install -y \
+    libimage-exiftool-perl \
+    imagemagick \
+    poppler-utils \
+    ffmpeg \
+    binwalk
+
 
 # Copy the script to the destination
 sudo cp "$SCRIPT_NAME" "$DESTINATION"
